f36b11b5c0b5eb6fe890c57c8e7ba15ba4ac5db6
lucasjotap <leiathemosthot@gmail.com>
2023-01-02 18:21:52 -0300

    New commit

---
+++ dict.py
@@ -0,0 +1,21 @@
+liss  = {"lissandra":
+    {
+        "nickname" : "liss",
+        "age" : 10,
+        "city" : "london" 
+    }
+}
+
+
+my_dict = {}
+
+my_dict["name"] = "Lissandra"
+my_dict["name"] = "Lissandra"
+my_dict["age"] = 10
+
+print(my_dict)
+print(liss["lissandra"]["nickname"])
+print(liss["lissandra"]["age"])
+print(liss["lissandra"])
+
+print(liss.values())
